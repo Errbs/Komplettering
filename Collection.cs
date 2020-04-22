8b0c36d1e3db998ff1c76e4822b0df0512711ce4
Errbs <54944859+Errbs@users.noreply.github.com>
2020-04-22 13:27:10 +0200

    Add project files.

---
+++ Collection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komplettering
+{
+    class Collection
+    {
+        List<string> pokemons = new List<string>() { "Squirtle", "Pikachu" };
+
+        public void AddPokemons(Pokemon pokemon)
+        {
+            pokemons.Add(pokemon)
+        }
+        public void ThrowPokemon(int i, Trainer a)
+        {
+            a.ThrowPokemon(i);
+
+        }
+    }
+}
---
+++ Trainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komplettering
+{
+    class Trainer
+    {
+        Collection pokemonCollection = new Collection();
+        public void ThrowPokemon(int i)
+        {
+            pokemonCollection.ThrowPokemon(i, this);
+        }
+    }
+}
